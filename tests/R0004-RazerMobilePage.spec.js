1a1f0a13cc968ce3088b6398e543e410c848a873
Chia Jiun Hong <chiajiunhong@gmail.com>
2022-11-15 13:21:39 +0800

    Added common login and logout function

--- tests/R0004-RazerMobilePage.spec.js
+++ tests/R0004-RazerMobilePage.spec.js
@@ -1,21 +1,27 @@
 import { test, expect } from '@playwright/test';
 import commonTestStep from './commonTestStep';
 
-
-
 test('Test 00.If Razer-Mobile page available', async ({ page }) => {
   await commonTestStep.MobileBaseTest(page);
 });
 
-test('Test 01.If Razer-Mobile page first item available', async ({ page }) => {
+test('Test 01.If Razer-Mobile page first item link is available', async ({ page }) => {
   await commonTestStep.MobileBaseTest(page);
   await page.locator('div[role="listitem"]:has-text("Razer Kishi V2 for iPhone Universal Mobile Gaming Controller for iPhone Learn Mo") a').first().click();
   await expect(page).toHaveURL('https://www.razer.com/sg-en/mobile-controllers/razer-kishi-v2-for-iphone');
-  await page.getByRole('heading', { name: 'RAZER KISHI V2 for iPhone' }).click();
-
-  await page.locator('div[role="listitem"]:has-text("Razer Hammerhead True Wireless Pro Take your audio immersion to new heights with")').getByRole('link', { name: 'Learn More >' }).click();
-  await expect(page).toHaveURL('https://www.razer.com/sg-en/mobile-earphones/razer-hammerhead-true-wireless-pro');
-  await page.getByRole('heading', { name: 'Razer Hammerhead True Wireless Pro' }).click();
-
+  await expect(page).toHaveScreenshot()
 });
+
+test('Test 02.If Razer-Mobile page second item link is available', async ({ page }) => {
+  await commonTestStep.MobileBaseTest(page);
+  await page.locator('div[role="listitem"]:has-text("Razer Kishi V2 for Android Universal Mobile Gaming Controller for Android Learn ")').getByRole('link', { name: 'Learn More >' }).click();
+  await expect(page).toHaveURL('https://www.razer.com/sg-en/mobile-controllers/razer-kishi-v2-for-android');
+});
+
+test('Test 03.If Razer-Mobile page third item is available', async ({ page }) => {
+  await commonTestStep.MobileBaseTest(page);
+  await page.locator('div[role="listitem"]:has-text("Razer Kishi Make your move with our award-winning universal mobile controller th")').getByRole('link', { name: 'Learn More >' }).click();
+  await expect(page).toHaveURL('https://www.razer.com/sg-en/mobile-controllers/razer-kishi');
+});
+
 
--- tests/commonTestStep.js
+++ tests/commonTestStep.js
@@ -1,6 +1,29 @@
 const { test, expect } = require("@playwright/test");
 
 module.exports = {
+  LoginBaseTest: async function (page) {
+    await test.step(`login`, async () => {
+        await expect(page).toHaveURL('https://www.razer.com/sg-en');
+        await page.getByRole('button', { name: 'Shopping cart' }).click();
+        await page.getByRole('link', { name: 'Log In' }).click();
+        await expect(page).toHaveURL('https://razerid.razer.com/?client_id=c839026333341c074b79b27149f3783215c27663&l=en&redirect=https://www.razer.com/sg-en/razerid-confirmation?url=/sg-en&iframe=0&force_login=1');
+        await page.getByLabel('Email Address').click();
+        await page.getByLabel('Email Address').fill('ocbcPlaywright@gmail.com');
+        await page.getByLabel('PasswordShow Password').click();
+        await page.getByLabel('PasswordShow Password').fill('playwright123');
+        await page.getByRole('button', { name: 'LOG IN' }).click();
+        await expect(page).toHaveURL('https://www.razer.com/sg-en/razerid-confirmation?url=/sg-en');
+        
+    } )
+  },
+
+  LooutBaseTest: async function (page) {
+    await test.step(`logout`, async () => {
+        await page.getByRole('link', { name: 'Log Out' }).click();
+        await expect(page).toHaveURL('https://www.razer.com/sg-en/services')
+    } )
+  },
+
   MobileBaseTest: async function (page) {
     await test.step(`Step 1 `, async () => {
         await page.goto('https://www.razer.com/sg-en/mobile');
