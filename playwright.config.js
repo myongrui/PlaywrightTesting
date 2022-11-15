a88be1de3068bcfde0adda7568923e06c9cd3e5f
JasonHeoJung <S10222947@connect.np.edu.sg>
2022-11-15 15:10:53 +0800

    Razer Store Test edited

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,10 @@
-const config = {
-  retries: process.env.CI ? 2 : 0, // set to 2 when running on CI
+// const config = {
+//   retries: process.env.CI ? 2 : 0, // set to 2 when running on CI
   
-  use: {
-      // Tell all tests to load signed-in state from 'storageState.json'.
-      storageState: 'auth.json',
-      trace: 'on-first-retry',
-    }
-  };
-  module.exports = config;
+//   use: {
+//       // Tell all tests to load signed-in state from 'storageState.json'.
+//       storageState: 'auth.json',
+//       trace: 'on-first-retry',
+//     }
+//   };
+//   module.exports = config;
