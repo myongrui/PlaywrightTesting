910b30ed20b6b5f36f7f27b413fecfe9af1c6cc7
Chia Jiun Hong <chiajiunhong@gmail.com>
2022-11-15 15:12:07 +0800

    Try

--- global-setup.js
+++ global-setup.js
@@ -1,11 +1,11 @@
-const { test } = require('@playwright/test');
+// const { test } = require('@playwright/test');
 
-module.exports = async config => {
-    const context = await browser.newContext({
-        storageState: "./auth.json"
-    })
-    const page = await context.newPage();
-  // Save signed-in state to 'storageState.json'.
-  await page.context().storageState({ path: 'storageState.json' });
-  await browser.close();
-};
+// module.exports = async config => {
+//     const context = await browser.newContext({
+//         storageState: "./auth.json"
+//     })
+//     const page = await context.newPage();
+//   // Save signed-in state to 'storageState.json'.
+//   await page.context().storageState({ path: 'storageState.json' });
+//   await browser.close();
+// };
