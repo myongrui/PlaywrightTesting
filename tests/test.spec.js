76981f7cc8c29f44e14e2a62b35d91444ae6aa03
myongrui <s10222891@connect.np.edu.sg>
2022-11-15 13:32:03 +0800

    fixing workflow

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,10 +1,6 @@
 const {test} = require('@playwright/test');
 
-test('test', async ({ browser }) => {
-    // const context = await browser.newContext({
-    //     storageState: "./auth.json"
-    // })
-    const page = await context.newPage();
+test('test', async ({ page }) => {
     await page.goto("https://www.razer.com/sg-en");
     await page.waitForTimeout(5000);
 });
