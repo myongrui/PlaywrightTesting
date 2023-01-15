98824dace2c6f09efb62c7d6982d72033386e98e
myongrui <s10222891@connect.np.edu.sg>
2023-01-15 15:13:20 +0800

    changed test cases to success and removed dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,14 +19,16 @@ const config: PlaywrightTestConfig = {
     //   use: { ...devices["Desktop Safari"] },
     // },
   ],
+  testMatch: ["**.spec.js"],
+  retries: 0,
+  reporter: 'html',
+  workers: process.env.CI ? 2 : undefined,
   use: {
     headless: true,
     // storageState: "./auth.json",
     navigationTimeout: 60000,
   },
-  testMatch: ["**.spec.js"],
-  retries: 0,
-  reporter: 'html',
+  
 };
 
 export default config;
