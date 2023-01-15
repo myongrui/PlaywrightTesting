fe04b36f68eccaf0ab3478007fb322315f3e75ba
myongrui <s10222891@connect.np.edu.sg>
2023-01-15 11:30:01 +0800

    changed config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,13 +22,12 @@ const config: PlaywrightTestConfig = {
   use: {
     headless: true,
     storageState: "./auth.json",
-    screenshot: "only-on-failure",
     actionTimeout: 60000,
     navigationTimeout: 60000,
   },
   testMatch: ["**.spec.js"],
   retries: 0,
-  // reporter: [["html", { outputFile: "testreports.html" }]],
+  reporter: [["html", { outputFile: "testreports.html" }]],
 };
 
 export default config;
