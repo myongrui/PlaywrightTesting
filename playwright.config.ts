cfc1e1422d69f954013e5a69748c56b34cf093c6
myongrui <s10222891@connect.np.edu.sg>
2023-01-15 14:43:58 +0800

    reverted back to playwright's test runner

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,33 +1,33 @@
-import { devices, PlaywrightTestConfig } from "@playwright/test";
+// import { devices, PlaywrightTestConfig } from "@playwright/test";
 
-const config: PlaywrightTestConfig = {
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "pixel",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
-  use: {
-    headless: true,
-    storageState: "./auth.json",
-    actionTimeout: 60000,
-    navigationTimeout: 60000,
-  },
-  testMatch: ["**.spec.js"],
-  retries: 0,
-  reporter: [["html", { outputFile: "testreports.html" }]],
-};
+// const config: PlaywrightTestConfig = {
+//   projects: [
+//     {
+//       name: "chromium",
+//       use: { ...devices["Desktop Chrome"] },
+//     },
+//     {
+//       name: "pixel",
+//       use: { ...devices["Pixel 5"] },
+//     },
+//     {
+//       name: "firefox",
+//       use: { ...devices["Desktop Firefox"] },
+//     },
+//     {
+//       name: "webkit",
+//       use: { ...devices["Desktop Safari"] },
+//     },
+//   ],
+//   use: {
+//     headless: true,
+//     storageState: "./auth.json",
+//     actionTimeout: 60000,
+//     navigationTimeout: 60000,
+//   },
+//   testMatch: ["**.spec.js"],
+//   retries: 0,
+//   reporter: [["html", { outputFile: "testreports.html" }]],
+// };
 
-export default config;
+// export default config;
