8bc7e7019c39e935907bedb0a5d8fd1d4479b32d
myongrui <s10222891@connect.np.edu.sg>
2023-01-16 19:22:27 +0800

    setup default configuration

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { devices, PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "pixel",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+  //testMatch: ["**.spec.js"],
+  retries: 0,
+  reporter: [['html',{outputFolder: './playwright-report/default'}]],
+  use: {
+    headless: false,
+    storageState: "./auth.json",
+    navigationTimeout: 60000,
+  },
+  
+};
+
+export default config;
