bbcfa8415e7f7e72703f717f94e086e64d4d6572
Chia Jiun Hong <78270716+sbboxs@users.noreply.github.com>
2023-01-28 08:30:07 +0800

    Fixing import data test case

--- tests/importData.spec.js
+++ tests/importData.spec.js
@@ -1,30 +1,31 @@
-import { test, expect } from '@playwright/test';
+import { describe, test, expect } from '@playwright/test';
 
 const fs = require('fs');
 const csv = require('csv-parser');
 
-let data = [];
-beforeAll(() => {
-    return new Promise((resolve, reject) => {
-        fs.createReadStream('data.csv')
-            .pipe(csv())
-            .on('data', (row) => {
-                data.push(row);
-            })
-            .on('end', resolve);
+describe('Importing data from CSV file', () => {
+    let data = [];
+    beforeAll(() => {
+        return new Promise((resolve, reject) => {
+            fs.createReadStream('data.csv')
+                .pipe(csv())
+                .on('data', (row) => {
+                    data.push(row);
+                })
+                .on('end', resolve);
+        });
+    });
+    
+    data.forEach((data) => {
+        test(`Fill form with data from CSV file ${data.name}`, async ({ page }) => {
+            await page.goto('https://example.com/form');
+            await page.fill('input[name="name"]', data.Name);
+            await page.fill('input[name="email"]', data.Email);
+            await page.fill('input[name="phone"]', data.Phone);
+            await page.click('button[type="submit"]');
+            await page.waitForSelector('#success');
+            const success = await page.$eval('#success', e => e.textContent);
+            expect(success).toBe('Form submitted successfully');
+        });
     });
 });
-
-data.forEach((data) => {
-    test(`Fill form with data from CSV file ${data.name}`, async ({ page }) => {
-        await page.goto('https://example.com/form');
-        await page.fill('input[name="name"]', data.Name);
-        await page.fill('input[name="email"]', data.Email);
-        await page.fill('input[name="phone"]', data.Phone);
-        await page.click('button[type="submit"]');
-        await page.waitForSelector('#success');
-        const success = await page.$eval('#success', e => e.textContent);
-        expect(success).toBe('Form submitted successfully');
-    });
-});
-
