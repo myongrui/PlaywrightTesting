9d918c05ad3ad2b476b71df82118c28f0a78642c
Chia Jiun Hong <78270716+sbboxs@users.noreply.github.com>
2023-01-28 08:34:27 +0800

    Fixing import DAtaaa

--- tests/importData.spec.js
+++ tests/importData.spec.js
@@ -1,31 +1,28 @@
-import { describe, test, expect } from '@playwright/test';
-
+import { test, expect } from '@playwright/test';
 const fs = require('fs');
 const csv = require('csv-parser');
 
-describe('Importing data from CSV file', () => {
-    let data = [];
-    beforeAll(() => {
-        return new Promise((resolve, reject) => {
-            fs.createReadStream('data.csv')
-                .pipe(csv())
-                .on('data', (row) => {
-                    data.push(row);
-                })
-                .on('end', resolve);
-        });
+let data = [];
+test(`Importing data from CSV file`, async () => {
+    await new Promise((resolve, reject) => {
+        fs.createReadStream('data.csv')
+            .pipe(csv())
+            .on('data', (row) => {
+                data.push(row);
+            })
+            .on('end', resolve);
     });
-    
-    data.forEach((data) => {
-        test(`Fill form with data from CSV file ${data.name}`, async ({ page }) => {
+
+    for(let i=0; i< data.length; i++){
+        test(`Fill form with data from CSV file ${data[i].name}`, async ({ page }) => {
             await page.goto('https://example.com/form');
-            await page.fill('input[name="name"]', data.Name);
-            await page.fill('input[name="email"]', data.Email);
-            await page.fill('input[name="phone"]', data.Phone);
+            await page.fill('input[name="name"]', data[i].Name);
+            await page.fill('input[name="email"]', data[i].Email);
+            await page.fill('input[name="phone"]', data[i].Phone);
             await page.click('button[type="submit"]');
             await page.waitForSelector('#success');
             const success = await page.$eval('#success', e => e.textContent);
             expect(success).toBe('Form submitted successfully');
         });
-    });
+    }
 });
